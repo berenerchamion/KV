d8749b9e7cd483d7f87875640812571bfa4c2a9a
Beren Erchamion <beren@houseofbeor.net>
2022-07-10 15:17:08 -0400

    first commit

---
+++ lib/kv.ex
@@ -0,0 +1,18 @@
+defmodule KV do
+  @moduledoc """
+  Documentation for `KV`.
+  """
+
+  @doc """
+  Hello beren.
+
+  ## Examples
+
+      iex> KV.hello()
+      :beren
+
+  """
+  def hello do
+    :beren
+  end
+end
